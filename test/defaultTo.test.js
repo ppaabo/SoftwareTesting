d53a37d7ab6ec1e930567bf4207252ae97edfefa
ppaabo <89841937+ppaabo@users.noreply.github.com>
2024-11-28 14:21:20 +0200

    Implemented unit tests for defaultTo.js

---
+++ test/defaultTo.test.js
@@ -0,0 +1,47 @@
+import defaultTo from "../src/defaultTo";
+
+describe("defaultTo", () => {
+  // Basic functionality
+  it("should return the value if it is not null, undefined or NaN", () => {
+    expect(defaultTo(1, 10)).toBe(1);
+    expect(defaultTo("lorem", "ipsum")).toBe("lorem");
+    expect(defaultTo(true, false)).toBe(true);
+  });
+
+  it("should return the default value, if value is undefined", () => {
+    expect(defaultTo(undefined, 10)).toBe(10);
+  });
+
+  it("should return the default value, if value is null", () => {
+    expect(defaultTo(null, 10)).toBe(10);
+  });
+
+  it("should return the default value, if value is NaN", () => {
+    const value = NaN;
+    // if (isNaN(value)) {
+    //   console.log("value is NaN");
+    // }
+    expect(defaultTo(value, 10)).toBe(10);
+  });
+
+  // Other types & edge cases
+  it("should handle arrays and objects as values", () => {
+    const value = [1, 2, 3];
+    const defaultValue = [4, 5, 6];
+    expect(defaultTo(value, defaultValue)).toBe(value);
+    expect(defaultTo(null, [1, 2, 3])).toEqual([1, 2, 3]);
+    expect(defaultTo(undefined, { a: 1 })).toEqual({ a: 1 });
+    expect(defaultTo({}, 10)).toEqual({});
+  });
+
+  it("should handle null and undefined as the default value", () => {
+    expect(defaultTo(1, null)).toBe(1);
+    expect(defaultTo(undefined, undefined)).toBe(undefined);
+  });
+
+  it("should handle special values (Infinity, -Infinity)", () => {
+    expect(defaultTo(Infinity, 10)).toBe(Infinity);
+    expect(defaultTo(-Infinity, 10)).toBe(-Infinity);
+    expect(defaultTo(null, Infinity)).toBe(Infinity);
+  });
+});
